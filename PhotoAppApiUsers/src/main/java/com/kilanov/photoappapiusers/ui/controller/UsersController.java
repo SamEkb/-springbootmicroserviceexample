15921ac5174a6d9a069f212747a16ba3eee4ca25
Kilanov <sam.kilanoff@gmail.com>
2023-08-22 21:53:55 +0500

    configurate routing for api gateway

--- PhotoAppApiUsers/src/main/java/com/kilanov/photoappapiusers/ui/controller/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/kilanov/photoappapiusers/ui/controller/UsersController.java
@@ -1,5 +1,6 @@
 package com.kilanov.photoappapiusers.ui.controller;
 
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UsersController {
+    private Environment environment;
+
+    public UsersController(Environment environment) {
+        this.environment = environment;
+    }
 
     @GetMapping("/status/check")
     public String status() {
-        return "Working...";
+        return "Working on " + environment.getProperty("local.server.port");
     }
 }
