9898623321b2a34a6da70532ddbf0fa9cabc8923
Kilanov <sam.kilanoff@gmail.com>
2023-09-01 21:06:52 +0500

    add resilience4j circuitbreaker configuration and default method for albums ws

--- UsersWebService/src/main/java/com/kilanov/userswebservice/service/AlbumsServiceClient.java
+++ UsersWebService/src/main/java/com/kilanov/userswebservice/service/AlbumsServiceClient.java
@@ -1,15 +1,22 @@
 package com.kilanov.userswebservice.service;
 
 import com.kilanov.userswebservice.ui.response.AlbumResponse;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @FeignClient(name = "albums-ws")
 public interface AlbumsServiceClient {
 
-    @GetMapping("/users/{id}/albumsss")
+    @GetMapping("/users/{id}/albums")
+    @CircuitBreaker(name = "albums-ws", fallbackMethod = "getAlbumsFallback")
     List<AlbumResponse> getAlbums(@PathVariable String id);
+
+    default List<AlbumResponse> getAlbumsFallback(String id, Throwable exception) {
+        return new ArrayList<>();
+    }
 }
